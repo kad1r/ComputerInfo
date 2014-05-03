265531c0656dc642a9ef770330101424f56a899a
kad1r <avcikadir@gmail.com>
2014-05-03 14:19:49 +0300

    first publish

---
+++ ComputerInfo/Form1.Designer.cs
@@ -0,0 +1,65 @@
+﻿namespace ComputerInfo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtLicence = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // txtLicence
+            // 
+            this.txtLicence.Location = new System.Drawing.Point(12, 12);
+            this.txtLicence.Multiline = true;
+            this.txtLicence.Name = "txtLicence";
+            this.txtLicence.ReadOnly = true;
+            this.txtLicence.Size = new System.Drawing.Size(361, 133);
+            this.txtLicence.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(385, 159);
+            this.Controls.Add(this.txtLicence);
+            this.Font = new System.Drawing.Font("Tahoma", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Create Licence";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtLicence;
+    }
+}
+
---
+++ ComputerInfo/Form1.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Management;
+using System.Net.NetworkInformation;
+using System.Security.Cryptography;
+using System.Text;
+using System.Windows.Forms;
+
+namespace ComputerInfo
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                var cpuid = string.Empty;
+                var hddserial = string.Empty;
+                var motherboardserial = string.Empty;
+                var macaddress = string.Empty;
+
+                #region cpu id
+
+                string sQuery = "SELECT ProcessorId FROM Win32_Processor";
+                ManagementObjectSearcher oManagementObjectSearcher = new ManagementObjectSearcher(sQuery);
+                ManagementObjectCollection oCollection = oManagementObjectSearcher.Get();
+                foreach (ManagementObject oManagementObject in oCollection)
+                {
+                    cpuid = (string)oManagementObject["ProcessorId"];
+                    break;
+                }
+
+                #endregion cpu id
+
+                #region mac address
+
+                NetworkInterface[] nics = NetworkInterface.GetAllNetworkInterfaces();
+                String sMacAddress = string.Empty;
+                foreach (NetworkInterface adapter in nics)
+                {
+                    if (sMacAddress == String.Empty)
+                    {
+                        IPInterfaceProperties properties = adapter.GetIPProperties();
+                        macaddress = adapter.GetPhysicalAddress().ToString();
+                    }
+                }
+
+                #endregion mac address
+
+                #region hdd serial
+
+                ManagementObject dsk = new ManagementObject(@"win32_logicaldisk.deviceid=""c:""");
+                dsk.Get();
+                hddserial = dsk["VolumeSerialNumber"].ToString();
+
+                #endregion hdd serial
+
+                #region motherboard serial
+
+                ManagementObjectSearcher mos = new ManagementObjectSearcher("SELECT * FROM Win32_BaseBoard");
+                ManagementObjectCollection moc = mos.Get();
+                foreach (ManagementObject mo in moc)
+                {
+                    motherboardserial = (string)mo["SerialNumber"];
+                    break;
+                }
+
+                #endregion motherboard serial
+
+                var code = cpuid.Trim() + " " + hddserial.Trim() + " " + motherboardserial.Trim() + " " + macaddress.Trim();
+                code = GetMd5Sum(code);
+                txtLicence.Text = code;
+            }
+            catch (Exception ex)
+            {
+                string error = ex.Message;
+                MessageBox.Show("Error: " + ex.Message);
+            }
+        }
+
+        public static string GetMd5Sum(string str)
+        {
+            Encoder enc = System.Text.Encoding.Unicode.GetEncoder();
+            byte[] unicodeText = new byte[str.Length * 2];
+            enc.GetBytes(str.ToCharArray(), 0, str.Length, unicodeText, 0, true);
+
+            MD5 md5 = new MD5CryptoServiceProvider();
+            byte[] result = md5.ComputeHash(unicodeText);
+
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < result.Length; i++)
+            {
+                sb.Append(result[i].ToString("X2"));
+            }
+
+            return sb.ToString();
+        }
+    }
+}
